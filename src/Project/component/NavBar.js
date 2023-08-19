11ddf3a11951bb5e5d2f3eb538519d5952eec7e7
masterajinkya <ajinkyagiri62@gmail.com>
2023-08-19 10:40:31 +0530

    updated

--- src/Project/component/NavBar.js
+++ src/Project/component/NavBar.js
@@ -71,10 +71,10 @@ function NavBar() {
 
     return (
         <>
-            <Box sx={{ flexGrow: 1 , marginTop: 5}} >
+            <Box sx={{ flexGrow: 1 , }} >
                 <AppBar position="static">
                     <Toolbar>
-                        <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                        <IconButton size="large" edge="start" color="inherit" aria-lambel="menu" sx={{ mr: 2 }}>
                         </IconButton>
                         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>Ajinkya Giri</Typography>
                         <Button onClick={() => setModal(true)} color="inherit">BuyNow</Button>
